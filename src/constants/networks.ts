88ea18c746bd3a8b70dcee94eeac027d51fb8b9d
DhruvJain1122 <amritkumarjain321@gmail.com>
2022-08-16 06:01:22 +0000

    feat: pokt network

--- src/constants/networks.ts
+++ src/constants/networks.ts
@@ -6,24 +6,27 @@ const INFURA_KEY = process.env.REACT_APP_INFURA_KEY
 if (typeof INFURA_KEY === 'undefined') {
   throw new Error(`REACT_APP_INFURA_KEY must be a defined environment variable`)
 }
-
-export const MAINNET_PROVIDER = new JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_KEY}`)
+const POKT_KEY = process.env.REACT_APP_POKT_KEY
+if (typeof POKT_KEY === 'undefined') {
+  throw new Error(`REACT_APP_POKT_KEY must be a defined environment variable`)
+}
+export const MAINNET_PROVIDER = new JsonRpcProvider(`https://eth-mainnet.gateway.pokt.network/v1/lb/${POKT_KEY}`)
 
 /**
  * These are the network URLs used by the interface when there is not another available source of chain data
  */
 export const RPC_URLS: { [key in SupportedChainId]: string } = {
-  [SupportedChainId.MAINNET]: `https://mainnet.infura.io/v3/${INFURA_KEY}`,
-  [SupportedChainId.RINKEBY]: `https://rinkeby.infura.io/v3/${INFURA_KEY}`,
-  [SupportedChainId.ROPSTEN]: `https://ropsten.infura.io/v3/${INFURA_KEY}`,
-  [SupportedChainId.GOERLI]: `https://goerli.infura.io/v3/${INFURA_KEY}`,
-  [SupportedChainId.KOVAN]: `https://kovan.infura.io/v3/${INFURA_KEY}`,
-  [SupportedChainId.OPTIMISM]: `https://optimism-mainnet.infura.io/v3/${INFURA_KEY}`,
-  [SupportedChainId.OPTIMISTIC_KOVAN]: `https://optimism-kovan.infura.io/v3/${INFURA_KEY}`,
-  [SupportedChainId.ARBITRUM_ONE]: `https://arbitrum-mainnet.infura.io/v3/${INFURA_KEY}`,
-  [SupportedChainId.ARBITRUM_RINKEBY]: `https://arbitrum-rinkeby.infura.io/v3/${INFURA_KEY}`,
-  [SupportedChainId.POLYGON]: `https://polygon-mainnet.infura.io/v3/${INFURA_KEY}`,
-  [SupportedChainId.POLYGON_MUMBAI]: `https://polygon-mumbai.infura.io/v3/${INFURA_KEY}`,
+  [SupportedChainId.MAINNET]: `https://eth-mainnet.gateway.pokt.network/v1/lb/${POKT_KEY}`,
+  [SupportedChainId.RINKEBY]: `https://eth-rinkeby.gateway.pokt.network/v1/lb/${POKT_KEY}`,
+  [SupportedChainId.ROPSTEN]: `https://eth-ropsten.gateway.pokt.network/v1/lb/${POKT_KEY}`,
+  [SupportedChainId.GOERLI]: `https://eth-goerli.gateway.pokt.network/v1/lb/${POKT_KEY}`,
+  [SupportedChainId.KOVAN]: `https://poa-kovan.gateway.pokt.network/v1/lb/${POKT_KEY}`,
+  [SupportedChainId.OPTIMISM]: `https://optimism-mainnet.gateway.pokt.network/v1/lb/${POKT_KEY}`,
+  [SupportedChainId.OPTIMISTIC_KOVAN]: `https://optimism-kovan.gateway.pokt.network/v1/lb/${POKT_KEY}`,
+  [SupportedChainId.ARBITRUM_ONE]: `https://abitrum-mainnet.gateway.pokt.network/v1/lb/${POKT_KEY}`,
+  [SupportedChainId.ARBITRUM_RINKEBY]: `https://abitrum-rinkeby.gateway.pokt.network/v1/lb/${POKT_KEY}`,
+  [SupportedChainId.POLYGON]: `https://poly-mainnet.gateway.pokt.network/v1/lb/${POKT_KEY}`,
+  [SupportedChainId.POLYGON_MUMBAI]: `https://poly-mumbai.gateway.pokt.network/v1/lb/${POKT_KEY}`,
   [SupportedChainId.CELO]: `https://forno.celo.org`,
   [SupportedChainId.CELO_ALFAJORES]: `https://alfajores-forno.celo-testnet.org`,
 }
